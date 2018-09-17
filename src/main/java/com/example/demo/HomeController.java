f11d9b1ac7a64764528dd03d3d978f319643c3de
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-17 17:29:35 +0600

    Spring Boot Thymeleaf View

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+
+
+@Controller
 public class HomeController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello";
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ModelAndView save(@ModelAttribute User user){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user-data");
+        modelAndView.addObject("user",user);
+        return  modelAndView;
     }
 }

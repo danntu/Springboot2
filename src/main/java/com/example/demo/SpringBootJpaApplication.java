cd7302f71ed86f1981235669d8ec36250f437494
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-18 10:19:33 +0600

    Spring Boot JPA

---
+++ src/main/java/com/example/demo/SpringBootJpaApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaApplication.class,args);
+    }
+}

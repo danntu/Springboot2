e8b3f44561a4daa9a20ecf89097ac74fdfc76e73
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-19 12:27:09 +0600

    Spring Boot JDBC

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserRecord,String> {
-}
+//package com.example.demo;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserRepository extends CrudRepository<UserRecord,String> {
+//}
--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,32 +1,32 @@
-package com.example.demo;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-
-    public List<UserRecord> getAllUsers(){
-        List<UserRecord> userRecords = new ArrayList<>();
-        userRepository.findAll().forEach(userRecords::add);
-        return userRecords;
-    }
-
-    public Optional<UserRecord> getUser(String id){
-        return userRepository.findById(id);
-    }
-
-    public void addUser(UserRecord userRecord){
-        userRepository.save(userRecord);
-    }
-
-    public void delete(String id){
-        userRepository.deleteById(id);
-    }
-}
+//package com.example.demo;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class UserService {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    public List<UserRecord> getAllUsers(){
+//        List<UserRecord> userRecords = new ArrayList<>();
+//        userRepository.findAll().forEach(userRecords::add);
+//        return userRecords;
+//    }
+//
+//    public Optional<UserRecord> getUser(String id){
+//        return userRepository.findById(id);
+//    }
+//
+//    public void addUser(UserRecord userRecord){
+//        userRepository.save(userRecord);
+//    }
+//
+//    public void delete(String id){
+//        userRepository.deleteById(id);
+//    }
+//}
